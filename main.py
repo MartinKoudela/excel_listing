a79d5d3e544f7219a425148af1263abaf83785a5
swa666rrass <koudela.m@icloud.com>
2025-05-13 13:07:24 +0200

    column hours

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import pandas as pd
+import openpyxl
 
-excel_file = 'listing_excel.xlsx'
-movies = pd.read_excel(excel_file)
+wb = openpyxl.load_workbook('listing_excel.xlsx')
+ws = wb.active
+
+print('Zaměstnanec:  ' + ws['A2'].value + '\nOdpracoval:  ' + str(ws['B2'].value) +' Hodin' + '\nV oddělení:  ' + ws['C2'].value)
+
+row_position = 2
+col_position = 2
+
+total_hours = ((int(ws.cell(row=row_position, column=col_position).value))+
+               (int(ws.cell(row=row_position+1, column=col_position).value))+
+               (int(ws.cell(row=row_position+2, column=col_position).value))+
+               (int(ws.cell(row=row_position+3, column=col_position).value)))
+ws.cell(row=2,column=4).value=total_hours
+wb.save('listing_excel.xlsx')
